56dd3c21094c86b362b817c0ca38d44bc7b7997a
Donato Garcia <616dona@gmail.com>
2022-11-23 22:33:46 -0300

    added 001-EventOnClick

---
+++ Fundamentos de la Web 2021/003-JS/001-EventOnClick/app.js
@@ -0,0 +1,11 @@
+const removeButton = (element) => {
+  element.style.display = "none";
+};
+
+const likeButton = () => {
+  alert("ninja was liked");
+};
+
+const loginText = (element) => {
+  element.textContent = "Logout";
+};
